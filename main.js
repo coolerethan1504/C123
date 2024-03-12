0e548d65946dc65c08d23ab24dd691af1f5cf4a6
coolerethan1504 <155672348+coolerethan1504@users.noreply.github.com>
2024-03-12 18:39:31 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+}
+
+function take_snapshot(){
+    save('myLipstickFilterImage.png');
+}
